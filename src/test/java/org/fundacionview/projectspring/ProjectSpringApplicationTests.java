a4e31c842851c0f76151cce88bd6150bcf1fda24
Rafael Arrieta <jose.arrieta@nordstrom.com>
2022-11-28 20:39:36 -0500

    Initial commit

---
+++ src/test/java/org/fundacionview/projectspring/ProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fundacionview.projectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
